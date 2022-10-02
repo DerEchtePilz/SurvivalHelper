8ac61e1919777dc9c8a9c5aeb144c08d77d1fbc0
DerEchtePilz <tlfm90@gmail.com>
2022-10-02 08:01:16 +0200

    initial commit

---
+++ src/main/kotlin/me/derechtepilz/survivalhelper/SurvivalHelper.kt
@@ -0,0 +1,11 @@
+package me.derechtepilz.survivalhelper
+
+import net.fabricmc.api.ModInitializer
+
+@Suppress("UNUSED")
+object SurvivalHelper : ModInitializer {
+    private const val MOD_ID = "survivalhelper"
+    override fun onInitialize() {
+        println("Example mod has been initialized.")
+    }
+}
