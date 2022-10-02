70959473bf086f6421a1401554af591f851af018
DerEchtePilz <tlfm90@gmail.com>
2022-10-02 14:54:20 +0200

    adding /shelp command

--- src/main/kotlin/me/derechtepilz/survivalhelper/SurvivalHelper.kt
+++ src/main/kotlin/me/derechtepilz/survivalhelper/SurvivalHelper.kt
@@ -3,21 +3,22 @@ package me.derechtepilz.survivalhelper
 import com.mojang.brigadier.CommandDispatcher
 import me.derechtepilz.survivalhelper.commands.HelpCommands
 import net.fabricmc.api.ClientModInitializer
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback
-import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
 import net.minecraft.command.CommandRegistryAccess
+import net.minecraft.server.command.CommandManager.RegistrationEnvironment
+import net.minecraft.server.command.ServerCommandSource
 
 object SurvivalHelper : ClientModInitializer {
 
     const val MOD_ID = "survivalhelper"
 
     override fun onInitializeClient() {
-        ClientCommandRegistrationCallback.EVENT.register(ClientCommandRegistrationCallback { dispatcher: CommandDispatcher<FabricClientCommandSource>, registryAccess: CommandRegistryAccess ->
-            registerCommands(dispatcher, registryAccess)
+        CommandRegistrationCallback.EVENT.register(CommandRegistrationCallback { dispatcher: CommandDispatcher<ServerCommandSource>, registryAccess: CommandRegistryAccess, registrationEnvironment: RegistrationEnvironment ->
+            registerCommands(dispatcher, registryAccess, registrationEnvironment)
         })
     }
 
-    private fun registerCommands(dispatcher: CommandDispatcher<FabricClientCommandSource>, registryAccess: CommandRegistryAccess) {
+    private fun registerCommands(dispatcher: CommandDispatcher<ServerCommandSource>, registryAccess: CommandRegistryAccess, registrationEnvironment: RegistrationEnvironment) {
         HelpCommands().register(dispatcher, registryAccess)
     }
 }
--- src/main/kotlin/me/derechtepilz/survivalhelper/commands/HelpCommands.kt
+++ src/main/kotlin/me/derechtepilz/survivalhelper/commands/HelpCommands.kt
@@ -3,23 +3,25 @@ package me.derechtepilz.survivalhelper.commands
 import com.mojang.brigadier.CommandDispatcher
 import com.mojang.brigadier.arguments.IntegerArgumentType
 import com.mojang.brigadier.context.CommandContext
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager
-import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource
-import net.minecraft.client.network.ClientPlayerEntity
+import com.mojang.brigadier.exceptions.CommandSyntaxException
 import net.minecraft.command.CommandRegistryAccess
 import net.minecraft.command.argument.ItemStackArgument
 import net.minecraft.command.argument.ItemStackArgumentType
+import net.minecraft.server.command.CommandManager
+import net.minecraft.server.command.ServerCommandSource
+import net.minecraft.server.network.ServerPlayerEntity
+import kotlin.math.min
 
 class HelpCommands {
 
-    fun register(dispatcher: CommandDispatcher<FabricClientCommandSource>, commandRegistryAccess: CommandRegistryAccess) {
-        dispatcher.register(ClientCommandManager.literal("shelp")
-                .then(ClientCommandManager.literal("building")
-                        .then(ClientCommandManager.literal("items")
-                                .then(ClientCommandManager.argument("item", ItemStackArgumentType.itemStack(commandRegistryAccess))
-                                        .then(ClientCommandManager.argument("amount", IntegerArgumentType.integer())
-                                                .executes { context: CommandContext<FabricClientCommandSource> ->
-                                                    execute(
+    fun register(dispatcher: CommandDispatcher<ServerCommandSource>, commandRegistryAccess: CommandRegistryAccess) {
+        dispatcher.register(CommandManager.literal("shelp")
+                .then(CommandManager.literal("building")
+                        .then(CommandManager.literal("items")
+                                .then(CommandManager.argument("item", ItemStackArgumentType.itemStack(commandRegistryAccess))
+                                        .then(CommandManager.argument("amount", IntegerArgumentType.integer())
+                                                .executes { context: CommandContext<ServerCommandSource> ->
+                                                    giveItem(
                                                         context.source,
                                                         ItemStackArgumentType.getItemStackArgument(context, "item"),
                                                         IntegerArgumentType.getInteger(context, "amount")
@@ -32,10 +34,21 @@ class HelpCommands {
         )
     }
 
-    private fun execute(clientCommandSource: FabricClientCommandSource, itemStackArgument: ItemStackArgument, amount: Int): Int {
-        val player: ClientPlayerEntity = clientCommandSource.player
-        player.inventory.insertStack(itemStackArgument.createStack(amount, false))
-        return 1
+    @kotlin.jvm.Throws(CommandSyntaxException::class)
+    private fun giveItem(serverCommandSource: ServerCommandSource, itemStackArgument: ItemStackArgument, amount: Int): Int {
+        val player: ServerPlayerEntity = serverCommandSource.playerOrThrow
+
+        var totalAmount = amount
+        val itemMaxStackSize = itemStackArgument.item.maxCount
+
+        while (totalAmount != 0) {
+            val itemAmount = min(totalAmount, itemMaxStackSize)
+            totalAmount -= itemAmount
+            val itemStack = itemStackArgument.createStack(itemAmount, false)
+            player.inventory.insertStack(itemStack)
+        }
+        player.inventory.markDirty()
+        return amount
     }
 
 }
